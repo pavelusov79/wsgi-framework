aaf16585862adf29b1ecb21c6cc4eb0a3e1689de
Pavel Usov <usov.p@mail.ru>
2020-12-16 10:37:55 +1000

    task_3

--- core/templates.py
+++ core/templates.py
@@ -1,18 +1,34 @@
-from jinja2 import Template
+# from jinja2 import Template
+# import os
+#
+#
+# def render(template_name, folder='templates', **kwargs):
+#     """
+#     :param template_name: имя шаблона
+#     :param folder: папка в которой ищем шаблон
+#     :param kwargs: параметры
+#     :return:
+#     """
+#     file_path = os.path.join(folder, template_name)
+#     # Открываем шаблон по имени
+#     with open(file_path, encoding='utf-8') as f:
+#         # Читаем
+#         template = Template(f.read())
+#     # рендерим шаблон с параметрами
+#     return template.render(**kwargs)
+
+"""
+Работа с шаблонами, паттерн INTERFACE
+Используем шаблонизатор jinja2
+"""
+from jinja2 import Template, FileSystemLoader
+from jinja2.environment import Environment
 import os
 
 
 def render(template_name, folder='templates', **kwargs):
-    """
-    :param template_name: имя шаблона
-    :param folder: папка в которой ищем шаблон
-    :param kwargs: параметры
-    :return:
-    """
-    file_path = os.path.join(folder, template_name)
-    # Открываем шаблон по имени
-    with open(file_path, encoding='utf-8') as f:
-        # Читаем
-        template = Template(f.read())
-    # рендерим шаблон с параметрами
-    return template.render(**kwargs)
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    tmpl = env.get_template(template_name)
+    return tmpl.render(**kwargs)
+
